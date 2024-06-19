bae3e6a90420fe5417d1fa8ddd8eafc8a0a7fbfb
Semyon Beloplotov <beloplotov.s@mail.ru>
2024-06-20 01:24:22 +0500

    Тут прописана структура страницы

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React, {useState} from "react";
 import './App.css';
+import getMonth from "./util";
+import CalendarHeader from "./components/CalendarHeader";
+import Sidebar from "./components/Sidebar";
+import Month from "./components/Month";
 
-function App() {
-  return (
-      <h1 className="text-5xl font-bold underline">
-        Hello world!
-      </h1>
-  );
+export default function App() {
+    const [currentMonth, setCurrentMonth] = useState(getMonth)
+    return (
+        <React.Fragment>
+            <div className="h-screen flex flex-columns">
+                <CalendarHeader />
+                <div className="flex flex-1">
+                    <Sidebar/>
+                    <Month month={currentMonth}/>
+
+                </div>
+            </div>
+        </React.Fragment>
+    );
 }
-
-export default App;
