cdd561c10d7528b5b7dfb1e538fd03cce4fd205e
Semyon Beloplotov <beloplotov.s@mail.ru>
2024-06-20 14:57:59 +0500

    Кнопка сайдбара для добавления задач в будущем

---
+++ src/components/CreateEventButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CreateEventButton(){
+    return (
+        <button className="border p-2 rounded-full flex items-center shadow-md hover:shadow-2xl">
+            <span className="pl-3 pr-4">Создать</span>
+        </button>
+    );
+};
