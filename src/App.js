c96760800e06602d332c1562c3d3cbf6ac81e999
Semyon Beloplotov <beloplotov.s@mail.ru>
2024-06-20 15:05:11 +0500

    Добавка контекста

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, {useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import './App.css';
 import getMonth from "./util";
 import CalendarHeader from "./components/CalendarHeader";
 import Sidebar from "./components/Sidebar";
 import Month from "./components/Month";
+import GlobalContext from "./context/GlobalContext";
 
 export default function App() {
     const [currentMonth, setCurrentMonth] = useState(getMonth)
+    const {monthIndex} = useContext(GlobalContext)
+    useEffect(() => {
+        setCurrentMonth(getMonth(monthIndex))
+    }, [monthIndex])
     return (
         <React.Fragment>
-            <div className="h-screen flex flex-columns">
+            <div className="h-screen flex flex-col">
                 <CalendarHeader />
                 <div className="flex flex-1">
                     <Sidebar/>
