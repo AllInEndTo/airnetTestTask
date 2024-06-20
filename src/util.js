3a54ca7e03d83d0f1124f1c2ce339c319f45ecab
Semyon Beloplotov <beloplotov.s@mail.ru>
2024-06-20 15:00:47 +0500

    Локализация дней на русский

--- src/util.js
+++ src/util.js
@@ -1,8 +1,9 @@
 import dayjs from "dayjs";
+require('dayjs/locale/ru')
 
 export default function getMonth(month = dayjs().month()) {
     const year = dayjs().year()
-    const firstDayOfTheMonth = dayjs(new Date(year, month, 1)).day()
+    const firstDayOfTheMonth = dayjs(new Date(year, month, 0)).day()
     let currentMonthCount = 0 - firstDayOfTheMonth
     const daysMatrix = new Array(5).fill([]).map(() => {
         return new Array(7).fill(null).map(() => {
